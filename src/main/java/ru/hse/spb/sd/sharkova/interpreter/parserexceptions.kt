46b9a5ea54b3148d8520a36eb0050d644df1c682
sharkovadarya <sharkovadarya@gmail.com>
2019-02-07 22:10:04 +0300

    removed extra files

--- src/main/java/ru/hse/spb/sd/sharkova/interpreter/parserexceptions.kt
+++
@@ -1,5 +0,0 @@
-package ru.hse.spb.sd.sharkova.interpreter
-
-abstract class ParserException(message: String) : Exception(message)
-
-class MismatchedQuotesException(message: String = "mismatched quotes") : ParserException(message)
