63c38a427c04102a86df60eec82a849d129ba832
sharkovadarya <sharkovadarya@gmail.com>
2019-03-19 22:26:35 +0300

    removed extra files to fix build error

--- src/main/java/ru/hse/spb/sd/sharkova/interpreter/Parser.kt
+++
@@ -1,8 +0,0 @@
-package ru.hse.spb.sd.sharkova.interpreter
-
-/**
- * Parser interface. Parses a line of input.
- * */
-interface Parser {
-    fun parseInput(input: String): List<String>
-}
--- src/main/java/ru/hse/spb/sd/sharkova/interpreter/stream/ErrorStream.kt
+++
@@ -1,3 +0,0 @@
-package ru.hse.spb.sd.sharkova.interpreter.stream
-
-class ErrorStream : OutputStream()
